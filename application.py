e48147533344d1ddd61f07d1b4514b35e29eb971
LEoo <kan91011@gmail.com>
2021-01-31 10:36:35 +0800

    make it better

--- application.py
+++ application.py
@@ -1,18 +1,29 @@
 """
 Object detection and image description on LINE bot
 """
+from datetime import datetime, timezone, timedelta
 import os
+import re
 import json
 import requests
 from flask import Flask, request, abort
 from azure.cognitiveservices.vision.computervision import ComputerVisionClient
+from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
+from azure.cognitiveservices.vision.face import FaceClient
 from msrest.authentication import CognitiveServicesCredentials
-from linebot import (LineBotApi, WebhookHandler)
-from linebot.exceptions import (InvalidSignatureError)
-from linebot.models import (MessageEvent, TextMessage, TextSendMessage,
-                            FlexSendMessage, ImageMessage)
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import (
+    MessageEvent,
+    TextMessage,
+    TextSendMessage,
+    FlexSendMessage,
+    ImageMessage,
+)
 from imgur_python import Imgur
 from PIL import Image, ImageDraw, ImageFont
+import time
+
 
 app = Flask(__name__)
 
